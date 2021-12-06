44b19ba0fa6558b6ddbcbac5c758e02564141422
jo-sol <zoixssm@gmail.com>
2021-12-06 23:26:08 +0900

    1206 push member 테스트값 추가, memberDTO.java 수정

--- app/docs/dbmodel/ear-member-data.sql
+++ app/docs/dbmodel/ear-member-data.sql
@@ -1,1 +1,11 @@
--- 회원 test data 작성하는 파일
+-- 회원 테스트 값 
+insert into member(member_no, nickname, email, password, created_dt, active)
+values(1, '상어', 'shark@test.com', password('1111'), '2021-12-06', 1);
+insert into member(member_no, nickname, email, password, created_dt, active)
+values(2, '사자', 'lion@test.com', password('1111'), '2021-12-06', 1);
+insert into member(member_no, nickname, email, password, created_dt, active)
+values(3, '선장', 'captain@test.com', password('1111'), '2021-12-07', 1);
+insert into member(member_no, nickname, email, password, created_dt, active)
+values(4, '다이아몬드', 'dia@test.com', password('1111'), '2021-12-07', 1);
+insert into member(member_no, nickname, email, password, created_dt, active)
+values(5, '사과', 'apple@test.com', password('1111'), '2021-12-08', 1);
