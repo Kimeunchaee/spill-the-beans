e5a4ac9aef29dd490ea5ef9cba3d011209574bb4
Kimeunchaee <eun00218@naver.com>
2021-12-08 14:47:30 +0900

    1208 push 로그인, 로그아웃 하는 중(header 수정) / sidebar 삭제

--- app/src/main/java/com/spill/beans/web/HomeController.java
+++ app/src/main/java/com/spill/beans/web/HomeController.java
@@ -1,15 +1,23 @@
 package com.spill.beans.web;
 
+import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import com.spill.beans.dto.MemberDTO;
 
 
 @Controller
 public class HomeController {
   @GetMapping("/home")
-  public ModelAndView home() {
+
+  public ModelAndView home(HttpSession session) {
+
+    MemberDTO member = ((MemberDTO) session.getAttribute("loginUser"));
+
     ModelAndView mv = new ModelAndView();
+
+    mv.addObject("loginUser", member);
     mv.addObject("pageTitle", "메인화면");
     mv.addObject("contentUrl", "home.jsp");
     mv.setViewName("template1");
