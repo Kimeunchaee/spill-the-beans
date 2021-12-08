0e90282ea4afe575e554cb2aabf3dfad12e8b314
Kimeunchaee <eun00218@naver.com>
2021-12-08 15:39:01 +0900

    1208 게시글 테스트값 고유번호 직접 지정으로 수정 완료 / 댓글 테스트값 추가 완료

--- app/docs/dbmodel/ear-board-data.sql
+++ app/docs/dbmodel/ear-board-data.sql
@@ -1,17 +1,17 @@
 -- 게시판 테스트 값 
-insert into board(title,content,viewCount,likeCount,commentCount,category_no,member_no) 
-values('상어가 무서워요','고등어 살려',0,0,0,2,1);
+insert into board(board_no, title,content,viewCount,likeCount,commentCount,category_no,member_no) 
+values(1, '상어가 무서워요','고등어 살려',0,0,0,2,1);
 
-insert into board(title,content,viewCount,likeCount,commentCount,category_no,member_no) 
-values('일진 사자','요즘 일진 무섭죠',0,0,0,2,2);
+insert into board(board_no, title,content,viewCount,likeCount,commentCount,category_no,member_no) 
+values(2, '일진 사자','요즘 일진 무섭죠',0,0,0,2,2);
 
-insert into board(title,content,viewCount,likeCount,commentCount,category_no,member_no) 
-values('선장님 멀미나요','멀미약 사야지',0,0,0,1,3);
+insert into board(board_no, title,content,viewCount,likeCount,commentCount,category_no,member_no) 
+values(3, '선장님 멀미나요','멀미약 사야지',0,0,0,1,3);
 
-insert into board(title,content,viewCount,likeCount,commentCount,category_no,member_no) 
-values('상사 생일 선물 문제','하하하',0,0,0,1,4);
+insert into board(board_no, title,content,viewCount,likeCount,commentCount,category_no,member_no) 
+values(4, '상사 생일 선물 문제','하하하',0,0,0,1,4);
 
-insert into board(title,content,viewCount,likeCount,commentCount,category_no,member_no) 
-values('사과 보다 귤','제주도 귤이 최고',0,0,0,3,5);
+insert into board(board_no, title,content,viewCount,likeCount,commentCount,category_no,member_no) 
+values(5, '사과 보다 귤','제주도 귤이 최고',0,0,0,3,5);
 
 
---
+++ app/docs/dbmodel/ear-comment-data.sql
@@ -0,0 +1,15 @@
+-- 게시판 테스트 값 
+insert into comment(board_no, member_no, content, isPublic, replyCount)
+values(1, 2, '비밀댓글이지롱', 2, 0);
+     
+insert into comment(board_no, member_no, content, isPublic, replyCount)
+values(1, 3, '상어는 내친구 하하하', 1, 0);
+ 
+insert into comment(board_no, member_no, content, isPublic, replyCount)
+values(1, 4, '비밀 비밀', 2, 0);
+ 
+insert into comment(board_no, member_no, content, isPublic, replyCount)
+values(1, 5, '고등어 자반 맛있는데,,,', 1, 0);
+ 
+
+
