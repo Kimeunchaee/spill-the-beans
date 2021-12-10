77503ec25f31c2c48a9644a9f6c40a25a9b1429c
jo-sol <zoixssm@gmail.com>
2021-12-10 13:41:57 +0900

    1210 push 관리자 추가(Auth, Member, Member-testData 수정)

--- app/docs/dbmodel/ear-member-data.sql
+++ app/docs/dbmodel/ear-member-data.sql
@@ -1,12 +1,16 @@
+-- 관리자 테스트 값 
+insert into member(member_no, nickname, email, password, created_dt, active)
+values(1, '임금님', 'king@test.com', password('1111'), '2021-12-05', 3);
+
 -- 회원 테스트 값 
 insert into member(member_no, nickname, email, password, created_dt, active)
-values(1, '상어', 'shark@test.com', password('1111'), '2021-12-06', 1);
+values(2, '상어', 'shark@test.com', password('1111'), '2021-12-06', 1);
 insert into member(member_no, nickname, email, password, created_dt, active)
-values(2, '사자', 'lion@test.com', password('1111'), '2021-12-06', 1);
+values(3, '사자', 'lion@test.com', password('1111'), '2021-12-06', 1);
 insert into member(member_no, nickname, email, password, created_dt, active)
-values(3, '선장', 'captain@test.com', password('1111'), '2021-12-07', 1);
+values(4, '선장', 'captain@test.com', password('1111'), '2021-12-07', 1);
 insert into member(member_no, nickname, email, password, created_dt, active)
-values(4, '다이아몬드', 'dia@test.com', password('1111'), '2021-12-07', 1);
+values(5, '다이아몬드', 'dia@test.com', password('1111'), '2021-12-07', 1);
 insert into member(member_no, nickname, email, password, created_dt, active)
-values(5, '사과', 'apple@test.com', password('1111'), '2021-12-08', 1);
+values(6, '사과', 'apple@test.com', password('1111'), '2021-12-08', 1);
 
