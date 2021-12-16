a9ee6bb06bee56d45bd285293dd25a8e97eb3cc5
lee-hyeyeon <lhy1415@knou.ac.kr>
2021-12-16 20:07:11 +0900

    1216 push ear-member-data.sql 회원데이터 추가 등록

--- app/docs/dbmodel/ear-member-data.sql
+++ app/docs/dbmodel/ear-member-data.sql
@@ -4,13 +4,34 @@ values(1, '임금님', 'king@test.com', password('1111'), '2021-12-05', 3);
 
 -- 회원 테스트 값 
 insert into member(member_no, nickname, email, password, created_dt, active)
-values(2, '상어', 'shark@test.com', password('1111'), '2021-12-06', 1);
+values(2, '상어', 'shark@naver.com', password('1111'), '2021-12-06', 1);
 insert into member(member_no, nickname, email, password, created_dt, active)
-values(3, '사자', 'lion@test.com', password('1111'), '2021-12-06', 1);
+values(3, '사자', 'lion@daum.net', password('1111'), '2021-12-06', 1);
 insert into member(member_no, nickname, email, password, created_dt, active)
-values(4, '선장', 'captain@test.com', password('1111'), '2021-12-07', 1);
+values(4, '선장', 'captain@gmail.com', password('1111'), '2021-12-07', 1);
 insert into member(member_no, nickname, email, password, created_dt, active)
-values(5, '다이아몬드', 'dia@test.com', password('1111'), '2021-12-07', 1);
+values(5, '다이아몬드', 'dia@naver.com', password('1111'), '2021-12-07', 1);
 insert into member(member_no, nickname, email, password, created_dt, active)
-values(6, '사과', 'apple@test.com', password('1111'), '2021-12-08', 1);
+values(6, '사과', 'apple@daum.net', password('1111'), '2021-12-08', 1);
+insert into member(member_no, nickname, email, password, created_dt, active)
+values(7, '이과장', 'lee@gmail.com', password('1111'), '2021-12-09', 1);
+insert into member(member_no, nickname, email, password, created_dt, active)
+values(8, '새우깡', 'ggang@naver.com', password('1111'), '2021-12-10', 1);
+insert into member(member_no, nickname, email, password, created_dt, active)
+values(9, '송탄미군햄버거', 'songtan@daum.net', password('1111'), '2021-12-11', 1);
+insert into member(member_no, nickname, email, password, created_dt, active)
+values(10, '의정부부대찌개', 'armi@naver.com', password('1111'), '2021-12-12', 1);
+insert into member(member_no, nickname, email, password, created_dt, active)
+values(11, '연안부두큰손', 'bighand@gmail.com', password('1111'), '2021-12-13', 1);
+insert into member(member_no, nickname, email, password, created_dt, active)
+values(12, '아이패드', 'ipad@gmail.com', password('1111'), '2021-12-14', 1);
+insert into member(member_no, nickname, email, password, created_dt, active)
+values(13, '갤럭시', 'galaxy@naver.com', password('1111'), '2021-12-15', 1);
+insert into member(member_no, nickname, email, password, created_dt, active)
+values(14, '삼성', 'samsung@daum.net', password('1111'), '2021-12-16', 1);
+insert into member(member_no, nickname, email, password, created_dt, active)
+values(15, '카카오', 'kakao@naver.com', password('1111'), '2021-12-16', 1);
+insert into member(member_no, nickname, email, password, created_dt, active)
+values(16, '테슬라', 'tesla@gmail.com', password('1111'), '2021-12-16', 1);
+
 
